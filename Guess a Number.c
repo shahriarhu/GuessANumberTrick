abd0f859d0cf8c8d326a9d8448f132d5ff80b10f
shahriarhu <34214399+shahriarhu@users.noreply.github.com>
2020-03-16 00:50:10 +0600

    Add files via upload

---
+++ Guess a Number.c
@@ -0,0 +1,240 @@
+#include<stdio.h>
+
+int main()
+
+{
+    char cn1='y',cn2='n',c;
+    printf("\n\n\n\n\n\n\nI will show you a trick, wanna see it? \n\nIf yes press 'y and Enter' or 'n and Enter' for no\n");
+    scanf("%c",&c);
+
+    if(c==cn1)
+    {
+        int ans;
+        printf("\nThink of a number between 1-127\n\n");
+        printf("After memorise it don't tell...... press Enter");
+
+        getch();
+
+        printf("\n\n\nNow is that number bellow here?\n\n");
+
+
+        //Card 1
+        printf("\n\n                              1,3,5,7,9,11,13,15\n\n");
+        printf("                            17,19,21,23,25,27,29,31\n\n");
+        printf("                            33,35,37,39,41,43,45,47\n\n");
+        printf("                            49,51,53,55,57,59,61,63\n\n");
+        printf("                            65,67,69,71,73,75,77,79\n\n");
+        printf("                            81,83,85,87,89,91,93,95\n\n");
+        printf("                         97,99,101,103,105,107,109,111\n\n");
+        printf("                        113,115,117,119,121,123,125,127\n\n\n");
+
+
+        printf("\nIf yes press 'y and Enter' or 'n and Enter' for no\n");
+
+        fflush(stdin);
+
+        scanf("%c",&c);
+
+        if(c==cn1)
+        {
+            ans=1;
+        }
+
+        else
+        {
+            ans=0;
+        }
+
+        printf("Again is that number bellow here?\n");
+
+        //Card 2
+        printf("\n\n\n                              2,3,6,7,10,11,14,15\n\n");
+        printf("                            18,19,22,23,26,27,30,31\n\n");
+        printf("                            34,35,38,39,42,43,46,47\n\n");
+        printf("                            50,51,54,55,58,59,62,63\n\n");
+        printf("                            66,67,70,71,74,75,78,79\n\n");
+        printf("                            82,83,86,87,90,91,94,95\n\n");
+        printf("                         98,99,102,105,106,107,110,111\n\n");
+        printf("                        114,115,118,119,122,123,126,127\n\n\n\n");
+
+        printf("\nIf yes press 'y and Enter' or 'n and Enter' for no\n");
+
+        fflush(stdin);
+
+        scanf("%c",&c);
+
+        if(c==cn1)
+        {
+            ans=ans+2;
+        }
+
+        else
+        {
+            ans=ans+0;
+        }
+
+        printf("Again is that number bellow here?\n");
+
+        //Card 3
+        printf("\n\n\n                              4,5,6,7,12,13,14,15\n\n");
+        printf("                            20,21,22,23,28,29,30,31\n\n");
+        printf("                            36,37,38,39,44,45,46,47\n\n");
+        printf("                            52,53,54,55,60,61,62,63\n\n");
+        printf("                            68,69,70,71,76,77,78,79\n\n");
+        printf("                            84,85,86,87,92,93,94,95\n\n");
+        printf("                        100,101,102,103,108,109,110,111\n\n");
+        printf("                        116,117,118,119,124,125,126,127\n\n\n\n");
+
+        printf("\nIf yes press 'y and Enter' or 'n and Enter' for no\n");
+
+        fflush(stdin);
+
+        scanf("%c",&c);
+
+        if(c==cn1)
+        {
+            ans=ans+4;
+        }
+
+        else
+        {
+            ans=ans+0;
+        }
+
+        printf("Again is that number bellow here?\n");
+
+        //Card 4
+        printf("\n\n\n                             8,9,10,11,12,13,14,15\n\n");
+        printf("                            24,25,26,27,28,29,30,31\n\n");
+        printf("                            40,41,42,43,44,45,46,47\n\n");
+        printf("                            56,57,58,59,60,61,62,63\n\n");
+        printf("                            72,73,74,75,76,77,78,79\n\n");
+        printf("                            88,89,90,91,92,93,94,95\n\n");
+        printf("                        104,105,106,107,108,109,110,111\n\n");
+        printf("                        120,121,122,123,124,125,126,127\n\n\n\n");
+
+        printf("\nIf yes press 'y and Enter' or 'n and Enter' for no\n");
+
+        fflush(stdin);
+
+        scanf("%c",&c);
+
+        if(c==cn1)
+        {
+            ans=ans+8;
+        }
+
+        else
+        {
+            ans=ans+0;
+        }
+
+        printf("Again is that number bellow here?\n");
+
+        //Card 5
+        printf("\n\n\n                            16,17,18,19,20,21,22,23\n\n");
+        printf("                            24,25,26,27,28,29,30,31\n\n");
+        printf("                            48,49,50,51,52,53,54,55\n\n");
+        printf("                            56,57,58,59,60,61,62,63\n\n");
+        printf("                            80,81,82,83,84,85,86,87\n\n");
+        printf("                            88,89,90,91,92,93,94,95\n\n");
+        printf("                        112,113,114,115,116,117,118,119\n\n");
+        printf("                        120,121,122,123,124,125,126,127\n\n\n\n");
+
+        printf("\nIf yes press 'y and Enter' or 'n and Enter' for no\n");
+
+        fflush(stdin);
+
+        scanf("%c",&c);
+
+        if(c==cn1)
+        {
+            ans=ans+16;
+        }
+
+        else
+        {
+            ans=ans+0;
+        }
+
+        printf("Again is that number bellow here?\n");
+
+        //Card 6
+        printf("\n\n\n                            32,33,34,35,36,37,38,39\n\n");
+        printf("                            40,41,42,43,44,45,46,47\n\n");
+        printf("                            48,49,50,51,52,53,54,55\n\n");
+        printf("                            56,57,58,59,60,61,62,63\n\n");
+        printf("                          96,97,98,99,100,101,102,103\n\n");
+        printf("                        104,105,106,107,108,109,110,111\n\n");
+        printf("                        112,113,114,115,116,117,118,119\n\n");
+        printf("                        120,121,122,123,124,125,126,127\n\n\n\n");
+
+        printf("\nIf yes press 'y and Enter' or 'n and Enter' for no\n");
+
+        fflush(stdin);
+
+        scanf("%c",&c);
+
+        if(c==cn1)
+        {
+            ans=ans+32;
+        }
+
+        else
+        {
+            ans=ans+0;
+        }
+
+        printf("Again is that number bellow here?\n");
+
+        //Card 7
+        printf("\n\n\n                            64,65,66,67,68,69,70,71\n\n");
+        printf("                            72,73,74,75,76,77,78,79\n\n");
+        printf("                            80,81,82,83,84,85,86,87\n\n");
+        printf("                            88,89,90,91,92,93,94,95\n\n");
+        printf("                          96,97,98,99,100,101,102,103\n\n");
+        printf("                        104,105,106,107,108,109,110,111\n\n");
+        printf("                        112,113,114,115,116,117,118,119\n\n");
+        printf("                        120,121,122,123,124,125,126,127\n\n\n\n");
+
+        printf("\nIf yes press 'y and Enter' or 'n and Enter' for no\n");
+
+        fflush(stdin);
+
+        scanf("%c",&c);
+
+        if(c==cn1)
+        {
+            ans=ans+64;
+        }
+
+        else
+        {
+            ans=ans+0;
+        }
+
+        printf("n\n\n\n\n\n\n\n\n                          Your Guessing Number is: %d\n\n\n\n\n\n\n",ans);
+
+        printf("                         I HOPE YOU ENJOYED THE TRICK\n\n\n");
+
+        printf("                                  THANK YOU\n\n\n");
+
+        printf("                                     -_-\n\n\n\n");
+
+    }
+
+
+
+
+    else if(c==cn2)
+    {
+        printf("\n\n\n\n\n\n\n\n\n\n                                 THANK YOU\n\n\n");
+
+        printf("                                    -_-\n\n\n\n\n\n\n\n\n\n\n");
+    }
+
+    getch();
+
+    return 0;
+}
+
